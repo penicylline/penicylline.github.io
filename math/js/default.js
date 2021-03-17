e1ff9c9a44ce8600db4f6f9f24f657692bbb6cdc
Bui Vu Hoai <penicylline@yahoo.com>
2021-03-17 17:32:00 +0700

    Update default.js

--- math/js/default.js
+++ math/js/default.js
@@ -1,5 +1,5 @@
-var MathConfig = {
-    max: 20,
+var TestConfig = {
+    max: 50,
     currentQuestion: null,
     questionCount: 0,
     wrongAnswers: 0
@@ -7,8 +7,13 @@ var MathConfig = {
 
 var QuestionType = {
     SUM_2: 'SUM_2',
+    SUM_1: 'SUM_1',
+    SUB_1: 'SUB_1',
     SUB_2: 'SUB_2'
-}
+};
+
+var QuestionTypeListFull = [QuestionType.SUM_1, QuestionType.SUM_2, QuestionType.SUB_1, QuestionType.SUB_2];
+var QuestionTypeList = [QuestionType.SUM_1, QuestionType.SUM_2];
 
 var QuestionData = function(type) {
     this.type = type;
@@ -19,41 +24,99 @@ var QuestionData = function(type) {
     this.result = null;
 }
 
-var Sum2Question = function() {
-    this.data = new QuestionData(QuestionType.SUM_2);
-    this.data.data[0] = Math.round(Math.random() * MathConfig.max);
-    this.data.data[1] = Math.round(Math.random() * MathConfig.max);
+var Sum1Question = function() {
+    this.data = new QuestionData(QuestionType.SUM_1);
+    this.data.data[0] = Math.round(Math.random() * TestConfig.max / 2);
+    this.data.data[1] = Math.round(Math.random() * TestConfig.max / 2);
     this.data.correctAnswer = this.data.data[0] + this.data.data[1];
 }
 
-Sum2Question.prototype.render = function(container) {
-    var content = "".concat(this.data.data[0]).concat(" + ").concat(this.data.data[1]);
-    container.text(content);
+Sum1Question.prototype.render = function(container1, container2) {
+    container1.text("".concat(this.data.data[0]).concat(" + ").concat(this.data.data[1]).concat(' = '));
+    container2.text('');
+}
+
+var Sum2Question = function() {
+    this.data = new QuestionData(QuestionType.SUM_2);
+    this.data.data[0] = Math.round(Math.random() * TestConfig.max / 2);
+    this.data.data[1] = Math.round(Math.random() * TestConfig.max / 2) + this.data.data[0];
+    this.data.correctAnswer = this.data.data[1] - this.data.data[0];
+}
+
+Sum2Question.prototype.render = function(container1, container2) {
+    container1.text("".concat(this.data.data[0]).concat(" + "));
+    container2.text(" = ".concat(this.data.data[1]));
+}
+
+var Sub1Question = function() {
+    this.data = new QuestionData(QuestionType.SUB_1);
+    this.data.data[0] = Math.round(Math.random() * TestConfig.max / 2);
+    this.data.data[1] = Math.round(Math.random() * TestConfig.max / 2) + this.data.data[0];
+    this.data.correctAnswer = this.data.data[1] - this.data.data[0];
+}
+
+Sub1Question.prototype.render = function(container1, container2) {
+    container1.text("".concat(this.data.data[1]).concat(' - ').concat(this.data.data[0]).concat(' = '));
+    container2.text('');
+}
+
+
+var Sub2Question = function() {
+    this.data = new QuestionData(QuestionType.SUB_1);
+    this.data.data[0] = Math.round(Math.random() * TestConfig.max / 2);
+    this.data.data[1] = Math.round(Math.random() * TestConfig.max / 2) + this.data.data[0];
+    this.data.correctAnswer = this.data.data[1] - this.data.data[0];
+}
+
+Sub2Question.prototype.render = function(container1, container2) {
+    container1.text("".concat(this.data.data[1]).concat(' - '));
+    container2.text(' = '.concat(this.data.data[0]));
+}
+
+function randomQuestion() {
+    var typeList = QuestionTypeList;
+    if ($('#chkSub').is(':checked')) {
+        typeList = QuestionTypeListFull;
+    }
+    var rand = Math.ceil(Math.random() * typeList.length);
+    var qType = typeList[rand];
+    switch(qType) {
+        case QuestionType.SUM_1:
+            return new Sum1Question();
+        case QuestionType.SUM_2:
+            return new Sum2Question();
+        case QuestionType.SUB_1:
+            return new Sub1Question();
+        case QuestionType.SUB_2:
+            return new Sub2Question();
+    }
+    return new Sum1Question();
 }
 
 function nextQuestion() {
-    var sum = new Sum2Question();
-    sum.render($('#question'));
-    MathConfig.currentQuestion = sum;
-    MathConfig.questionCount++;
-    $('#qCount').text(MathConfig.questionCount);
+    var sum = randomQuestion();
+    sum.render($('#beforeQ'), $('#afterQ'));
+    TestConfig.currentQuestion = sum;
+    TestConfig.questionCount++;
+    $('#qCount').text(TestConfig.questionCount);
     $('#answer').val('');
+    $('#answer').focus();
 }
 
 $(document).ready(function (){
     nextQuestion();
 
     $('#btnCheck').click(function() {
-        if (MathConfig.currentQuestion == null) {
+        if (TestConfig.currentQuestion == null) {
             return;
         }
         var val = $('#answer').val();
-        if (MathConfig.currentQuestion.data.correctAnswer == val) {
+        if (TestConfig.currentQuestion.data.correctAnswer == val) {
             alert('Correct!!!');
             nextQuestion();
         } else {
-            MathConfig.wrongAnswers++;
-            $('#wrongCounts').text(MathConfig.wrongAnswers)
+            TestConfig.wrongAnswers++;
+            $('#wrongCounts').text(TestConfig.wrongAnswers)
             alert('Wrong!!!');
         }
     });
